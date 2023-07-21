e32b7b500c3bae2092c185bda542fec5865b1e56
ArsShon2000 <arsenkaribaev98@gmail.com>
2023-07-21 15:05:02 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+﻿#include "mainwindow.h"
+#include "qopenglskyboxwidget.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+//    MainWindow w;
+//    w.show();
+    QOpenGLSkyboxWidget widget(":/front.jpg",
+                               ":/back.jpg",
+                               ":/top.jpg",
+                               ":/bottom.jpg",
+                               ":/left.jpg",
+                               ":/right.jpg");
+    widget.show();
+    return a.exec();
+}
